48ae451c25ebe89ad8292a4f796bd5d22480067e
Nizwm Borgoyari <borgoyarinizwm@gmail.com>
2022-05-07 16:34:57 +0530

    New components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
-import katie from "./images/katie-zaferes.png";
 import "./style.css";
+import data from "./data";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,12 @@ import star from "../images/star.png";
 export default function Card(props) {
   return (
     <div className="card">
-      <img className="card-image" src={props.img} />
+      <img className="card-image" src={`../images/${props.img}`} />
       <div className="card-stats">
         <img className="card-star" src={star} />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount})*</span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p>
