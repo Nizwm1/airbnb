d54376c18a2c85ded0fa560142bfe076687352df
Nizwm Borgoyari <borgoyarinizwm@gmail.com>
2022-05-07 16:50:51 +0530

    New changes in props

--- src/components/card.js
+++ src/components/card.js
@@ -4,15 +4,15 @@ import star from "../images/star.png";
 export default function Card(props) {
   return (
     <div className="card">
-      <img className="card-image" src={`../images/${props.img}`} />
-      <div className="card-stats">
-        <img className="card-star" src={star} />
+      <img src={`../images/${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <img src={star} className="card--star" />
         <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount})*</span>
+        <span className="gray">({props.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
         <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
